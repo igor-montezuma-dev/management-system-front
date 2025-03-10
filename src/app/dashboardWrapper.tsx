6c8f6c2853f0565613f80814e75b22066d3d5203
IgorMontezuma20 <igorminranda17@gmail.com>
2025-03-10 00:01:01 -0300

    feat: Create sidebar component structure

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,11 +1,13 @@
 import { ReactNode } from "react";
 import Navbar from "./(components)/Navbar";
+import Sidebar from "./(components)/Sidebar";
 
 function DashboardWrapper({ children }: { children: ReactNode }) {
   return (
     <div className="flex max-h-screen w-full bg-gray-50 text-gray-900">
       {/* Sidebar */}
-      Sidebar
+
+      <Sidebar/>
       <main
         className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}
       >
