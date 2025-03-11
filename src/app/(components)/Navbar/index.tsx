7e5e706584d568f17c143f8d0a861b0fba7fe338
IgorMontezuma20 <igorminranda17@gmail.com>
2025-03-11 00:22:43 -0300

    feat: Dark mode implementation

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -1,11 +1,32 @@
-import { SearchIcon, SettingsIcon } from "lucide-react";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
+import {
+  MenuIcon,
+  MoonIcon,
+  SearchIcon,
+  SettingsIcon,
+  SunIcon,
+} from "lucide-react";
 import Link from "next/link";
 
 function Navbar() {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
   return (
     <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
       {/* Serach */}
       <div className="flex items-center gap-8">
+        {!isSidebarCollapsed ? null : (
+          <button
+            onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}
+          >
+            <MenuIcon className="size-8 dark:text-white" />
+          </button>
+        )}
         <div className="relative flex h-min w-[200px]">
           <SearchIcon className="absolute left-[4px] top-1/2 mr-2 size-5 -translate-y-1/2 transform cursor-pointer dark:text-white" />
           <input
@@ -16,9 +37,27 @@ function Navbar() {
         </div>
       </div>
       <div className="flex items-center">
+        <button
+          onClick={() => dispatch(setIsDarkMode(!isDarkMode))}
+          className={
+            isDarkMode
+              ? `rounded p-2 dark:hover:bg-gray-700`
+              : `rounded p-2 hover:bg-gray-100`
+          }
+        >
+          {isDarkMode ? (
+            <SunIcon className="size-6 dark:text-white" />
+          ) : (
+            <MoonIcon className="size-6 dark:text-white" />
+          )}
+        </button>
         <Link
           href="/settings"
-          className="h-min w-min rounded p-2 hover:bg-gray-100"
+          className={
+            isDarkMode
+              ? `h-min w-min rounded p-2 dark:hover:bg-gray-700`
+              : `h-min w-min rounded p-2 hover:bg-gray-100`
+          }
         >
           <SettingsIcon className="size-6 dark:text-white" />
         </Link>
