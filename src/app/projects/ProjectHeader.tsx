1684610560b41002ff468064e7045f9c53d95f42
IgorMontezuma20 <igorminranda17@gmail.com>
2025-03-14 15:40:29 -0300

    feat: Create Tab button component

--- src/app/projects/ProjectHeader.tsx
+++ src/app/projects/ProjectHeader.tsx
@@ -1,6 +1,14 @@
 "use client";
 
 import Header from "@/components/Header";
+import {
+  ClockIcon,
+  FilterIcon,
+  Grid3x3Icon,
+  ListIcon,
+  Share2Icon,
+  TableIcon,
+} from "lucide-react";
 
 import { useState } from "react";
 
@@ -16,7 +24,74 @@ const ProjectHeader = ({ activeTab, setActiveTab }: Props) => {
       <div className="pt-6lg:pb-4 pb-5 lg:pt-8">
         <Header name="Product Design Development" />
       </div>
+      <div className="flex flex-wrap-reverse gap-2 border-y border-gray-200 pb-[8px] pt-2 dark:border-stroke-dark md:items-center">
+        <div className="flex flex-1 items-center gap-2 md:gap-4">
+          <TabButton
+            name="Board"
+            icon={<Grid3x3Icon className="size-5" />}
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
+          <TabButton
+            name="Lista"
+            icon={<ListIcon className="size-5" />}
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
+          <TabButton
+            name="Linha do tempo"
+            icon={<ClockIcon className="size-5" />}
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
+          <TabButton
+            name="Tabela"
+            icon={<TableIcon className="size-5" />}
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
+        </div>
+        <div className="flex items-center gap-2">
+          <button className="text-gray-500 hover:text-gray-600 dark:text-gray-300 dark:hover:text-neutral-500">
+            <FilterIcon className="size-5" />
+          </button>
+          <button className="text-gray-500 hover:text-gray-600 dark:text-gray-300 dark:hover:text-neutral-500">
+            <Share2Icon className="size-5" />
+          </button>
+          <div className="relative">
+            <input
+              type="text"
+              placeholder="Procure por uma task"
+              className="rounded-md border py-1 pl-10 pr-4 focus:outline-none dark:border-dark-secondary dark:bg-dark-secondary dark:text-white"
+            />
+            <Grid3x3Icon className="absolute left-3 top-2 size-4 text-gray-400 dark:text-neutral-500" />
+          </div>
+        </div>
+      </div>
     </div>
+  );
+};
+
+type TabButtonProps = {
+  name: string;
+  icon: React.ReactNode;
+  setActiveTab: (tabName: string) => void;
+  activeTab: string;
+};
+
+const TabButton = ({ name, icon, setActiveTab, activeTab }: TabButtonProps) => {
+  const isActive = activeTab === name;
+
+  return (
+    <button
+      className={`relative flex items-center gap-2 px-1 py-2 text-gray-500 after:absolute after:-bottom-[9px] after:left-0 after:h-[1px] after:w-full hover:text-blue-600 dark:text-neutral-500 dark:hover:text-white sm:px-2 lg:px-4 ${
+        isActive ? "text-blue-600 after:bg-blue-600 dark:text-white" : ""
+      }`}
+      onClick={() => setActiveTab(name)}
+    >
+      {icon}
+      {name}
+    </button>
   );
 };
 
